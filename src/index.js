25e6ceeb63b107dff9f3be3fe534df54469ad901
Lucyna Aleksandra <lucyaleksa@gmail.com>
2019-02-05 12:35:24 -0500

    adding animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import "./index.scss"
+
+// LAZY LOADING
+document.addEventListener("DOMContentLoaded", function() {
+  var sections = [].slice.call(document.querySelectorAll(".la-animatable"))
+
+  if ("IntersectionObserver" in window) {
+    let sectionObserver = new IntersectionObserver(
+      function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("la-animatable--animate")
+            //sectionObserver.unobserve(entry.target)
+          } else {
+            entry.target.classList.remove("la-animatable--animate")
+          }
+        })
+      },
+      {
+        rootMargin: "0px 0px -500px 0px",
+      }
+    )
+
+    sections.forEach(function(section) {
+      sectionObserver.observe(section)
+    })
+  }
+})
+
+// // MENU
+// window.openNav = () => {
+//   document.querySelector(".la-menu").classList.add("la-nav__menu--visible")
+//   //document.body.style.overflow = "hidden"
+// }
+
+// window.closeNav = () => {
+//   document.querySelector(".la-menu").classList.remove("la-nav__menu--visible")
+//   document.body.style.overflow = "visible"
+// }
+
+// // STICKY NAV
+// $(document).ready(function() {
+//   $("#la-nav__navbar").sticky({
+//     topSpacing: 0,
+//     zIndex: 15,
+//   })
+// })
